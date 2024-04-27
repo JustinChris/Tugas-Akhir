fd1fee886fba33c0fd64048d9bede931307edd74
Justin Christian <justin.christian@gdn-commerce.com>
2024-04-27 10:42:42 +0700

    add bootstrap and dropdown

--- src/views/components/navbar.vue
+++ src/views/components/navbar.vue
@@ -2,10 +2,23 @@
     <nav>
         <img src="../../assets/images/onetripid-logo.png" alt="">
         <ul>
-            <li><a href="#">Accommodation</a></li>
-            <li><a href="#">Offer</a></li>
-            <li><a href="#">Blog</a></li>
-            <li><a href="#">Contact Us</a></li>
+            <div class="d-flex list-unstyled">
+                <li>
+                    <div class="dropdown">
+                        <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown button
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li><a href="#">Offer</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </div>
         </ul>
     </nav>
 </template>
@@ -26,9 +39,9 @@ li > a {
     color: black;
 }
 
-ul {
+/* ul {
     display: flex;
-}
+} */
 
 ul > li{
     list-style: none;
